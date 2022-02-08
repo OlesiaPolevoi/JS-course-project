427acc9bbcbbc445856ee812cb9014bc261416f4
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-08 12:42:40 -0500

    started carousel - our services JS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import modal from "./modules/modal";
 import slider from "./modules/slider";
+import carousel from "./modules/carousel";
 
 modal();
 slider();
+carousel();
---
+++ src/modules/carousel.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const carousel = () => {
+  console.log("hello carousel");
+};
+
+export default carousel;
